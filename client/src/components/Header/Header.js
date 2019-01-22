e60f41b21d0751afbde30f21d6d777977ccd4274
Andy Wilk <andrewdwilk@gmail.com>
2019-01-22 10:09:15 -0500

    building landing page

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav className="navbar bg-light">
+            <a className="navbar-brand text-dark" href="#">BookFinder</a>
+        </nav>
+    )
+};
+
+export default Header;
