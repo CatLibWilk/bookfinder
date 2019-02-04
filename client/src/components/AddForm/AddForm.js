1f7b7359cf1e22a3d22bc39459b40ae67d68b5a5
Andy Wilk <andrewdwilk@gmail.com>
2019-02-04 13:54:15 -0500

    input and button submit functions set up for addform

--- client/src/components/AddForm/AddForm.js
+++ client/src/components/AddForm/AddForm.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
-const AddForm = ({ width }) => {
+const AddForm = ({ width, funct, subFunct }) => {
     return (
         <div className={width ? `${width} mx-auto mt-3` : 'col-md-6 mx-auto mt-3'}>
             
             <form>
             <div class="form-group">
                 <label for="title-input">Title</label>
-                <input type="text" class="form-control" id="title-input" placeholder="Enter Title"></input>
+                <input type="text" class="form-control" id="title-input" placeholder="Enter Title" onChange={funct}></input>
                 
             </div>
 
             <div class="form-group">
                 <label for="author-input">Author</label>
-                <input type="text" class="form-control" id="author-input" placeholder="Enter Author Name"></input>
+                <input type="text" class="form-control" id="author-input" placeholder="Enter Author Name" onChange={funct}></input>
             </div>
 
-            <button id="add-btn" type="submit" class="btn">Submit</button>
+            <button id="add-btn" type="submit" class="btn" onClick={subFunct}>Submit</button>
             </form>
         </div>
     )
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,6 +3,13 @@ import List from '../../components/List';
 import AddForm from '../../components/AddForm';
 
 class Home extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            title_input: '',
+            author_input: ''
+        }
+    }
     componentDidMount(){
         setTimeout(load, 1000);
         
@@ -11,14 +18,34 @@ class Home extends Component {
           document.querySelector('#home-background').classList.add('wrapper-loaded');
 
           
-        }
+        }  
     }
+
+    handleInput = (e) => {
+
+        switch (e.target.id){
+            case "title-input":
+                this.setState( {title_input: e.target.value} );
+                break;
+            case "author-input":
+            this.setState( {author_input: e.target.value} );
+                break;
+        }
+
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('sub call')
+        console.log(this.state)
+    }
+
     render(){
         return (
             <div className='row'>
                 <div id='home-background' className='col-12 text-center'>
 
-                    <AddForm width={'col-md-6'}/>
+                    <AddForm width={'col-md-6'} funct={this.handleInput} subFunct={this.handleSubmit}/>
 
                     <List />
                 </div>
