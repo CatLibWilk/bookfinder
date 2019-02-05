2bc399f5d4a123014ed725d68d953164eb295ee6
Andy Wilk <andrewdwilk@gmail.com>
2019-02-05 17:21:05 -0500

    working on table to item align

--- client/src/components/List/List.js
+++ client/src/components/List/List.js
@@ -3,7 +3,10 @@ import React from 'react';
 const List = (props) => {
     return (
             <div id='list' className='col-md-6 mx-auto mt-3 p-2'>
+            <table className='col-12'>
+
                 {props.children}
+            </table>
             </div>
 
     )
--- client/src/components/ListItem/ListItem.js
+++ client/src/components/ListItem/ListItem.js
@@ -13,11 +13,22 @@ class ListItem extends Component{
     render(){
     
         return (
-                <div id='listItem' className="clearfix mt-md-2 mb-md-2 p-md-1 bg-light">
-                    <h2 className="float-md-left">{this.props.title}</h2>
-                    <h2 className="float-md-left ml-md-5">{this.props.author}</h2>
-                    <div className="float-right col-md-2" id={this.props.priority === 1 ? 'green' : this.props.priority === 2 ? 'yellow' : this.props.priority === 3 ? 'red' : 'grey'} onClick={this.props.delFunct}><h1 id='color-indicator-x'>X</h1></div>
-                </div>
+            <div className="clearfix mt-md-2 mb-md-2 p-md-1 listItem">
+                <tr>
+                    <td className='col-4'>
+                        <h2 className="float-md-left">{this.props.title}</h2>
+                    </td>
+
+                    <td className='col-4'>
+                        <h2 className="float-md-left ml-md-5">{this.props.author}</h2>
+                    </td>
+
+                    <td className='col-4'>
+                        <div className="float-right col-md-2" id={this.props.priority === 1 ? 'green' : this.props.priority === 2 ? 'yellow' : this.props.priority === 3 ? 'red' : 'grey'} onClick={this.props.delFunct}><h1 id='color-indicator-x'>X</h1></div>
+                    </td>
+
+                </tr>
+            </div>
         )
     }
 };
