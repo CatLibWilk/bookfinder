1cb4d908af57e1f80ef098cf0d68f724ce73db33
Andy Wilk <andrewdwilk@gmail.com>
2019-02-06 17:28:52 -0500

    added box shadow and border radius"

--- client/src/components/ListItem/ListItem.js
+++ client/src/components/ListItem/ListItem.js
@@ -14,20 +14,9 @@ class ListItem extends Component{
     
         return (
             <div className="clearfix mt-md-2 mb-md-2 p-md-1 listItem">
-                <tr>
-                    <td className='col-4'>
-                        <h2 className="float-md-left">{this.props.title}</h2>
-                    </td>
-
-                    <td className='col-4'>
-                        <h2 className="float-md-left ml-md-5">{this.props.author}</h2>
-                    </td>
-
-                    <td className='col-4'>
-                        <div className="float-right col-md-2" id={this.props.priority === 1 ? 'green' : this.props.priority === 2 ? 'yellow' : this.props.priority === 3 ? 'red' : 'grey'} onClick={this.props.delFunct}><h1 id='color-indicator-x'>X</h1></div>
-                    </td>
-
-                </tr>
+                <p className="float-md-left">{this.props.title}</p>
+                <p className="float-md-left ml-md-5">{this.props.author}</p>
+                <div className="float-right col-md-2 color-box" id={this.props.priority === 1 ? 'green' : this.props.priority === 2 ? 'yellow' : this.props.priority === 3 ? 'red' : 'grey'} onClick={this.props.delFunct}><h1 id='color-indicator-x'>X</h1></div>
             </div>
         )
     }
