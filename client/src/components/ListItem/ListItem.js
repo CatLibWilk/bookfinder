d3325032325ccd220a129fb8ed83534c6975a120
Andy Wilk <andrewdwilk@gmail.com>
2019-02-06 17:34:27 -0500

    fixed listitem grid problem

--- client/src/components/ListItem/ListItem.js
+++ client/src/components/ListItem/ListItem.js
@@ -14,8 +14,12 @@ class ListItem extends Component{
     
         return (
             <div className="clearfix mt-md-2 mb-md-2 p-md-1 listItem">
-                <p className="float-md-left">{this.props.title}</p>
-                <p className="float-md-left ml-md-5">{this.props.author}</p>
+                <div className="col-md-5 float-md-left">
+                    <p className='float-md-left'>{this.props.title}</p>
+                </div>
+                <div className="col-md-5 float-md-left">
+                    <p>{this.props.author}</p>
+                </div>
                 <div className="float-right col-md-2 color-box" id={this.props.priority === 1 ? 'green' : this.props.priority === 2 ? 'yellow' : this.props.priority === 3 ? 'red' : 'grey'} onClick={this.props.delFunct}><h1 id='color-indicator-x'>X</h1></div>
             </div>
         )
